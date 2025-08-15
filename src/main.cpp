ea2b8d3531fb4ae026d411f93cc3c153f5e2feff
Alexander Eremenchuk <alexer92@yandex.ru>
2025-08-15 14:09:41 +0300

    feat: initialize SFML application skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/WindowStyle.hpp>
 
 int main() {
-    std::cout << "Timber game" << std::endl;
+    // Create a video mode object
+    sf::VideoMode vm(1920, 1080);
+
+    // Create and open a window for the game
+    sf::RenderWindow window(vm, "Timber", sf::Style::Default);
+
+    // Main game loop
+    while (window.isOpen()) {
+        // Event handling
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if ((event.type == sf::Event::Closed) ||
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
+                window.close();
+            }
+        }
+
+        // Clear everything from the last frame
+        window.clear();
+
+        // Draw game scene
+
+        // Show everything we just draw
+        window.display();
+    }
+
     return 0;
 }
